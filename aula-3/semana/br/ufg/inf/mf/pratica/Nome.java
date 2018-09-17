0d50c21424a9c103eb0390b300d8cfac44463afb
Rodrigo <cvc.francisco@gmail.com>
2018-09-17 20:39:49 -0300

    Criação da classe

---
+++ aula-3/semana/br/ufg/inf/mf/pratica/Nome.java
@@ -0,0 +1,12 @@
+package br.ufg.inf.mf.pratica;
+import java.util.Locale;
+
+public class Nome {
+
+
+ public static void main(String args[]) { 
+  Locale mvLocale = Locale.getDefault();
+  System.out.println(mvLocale);
+ }
+}
+
